6c212e8531dc6efda9b73a9c28c19f2ff9ae2613
Deivid Veloso <veloso.deivid@gmail.com>
2018-01-28 14:28:28 -0200

    install eslint and editor

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
-    extends: ['airbnb', 'plugin:jest/recommended'],
-    plugins: ['jest'],
-    rules: {
-      'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
-      'react/prefer-stateless-function': 0,
-      'import/no-extraneous-dependencies': 0,
-      'import/no-unresolved': 0,
-      'import/extensions': 0,
-      'jsx-a11y/label-has-for': 'off',
-      'comma-dangle': 'off',
-      'object-curly-newline': 'off',
-      'react/forbid-prop-types': 'off',
-      'linebreak-style': 0,
-      'import/prefer-default-export': 'off',
-      'arrow-parens': 'off',
-      'no-nested-ternary': 'off',
-      'no-plusplus': 'off',
-      'no-underscore-dangle': [2, { allowAfterThis: true }],
-      'no-console': 0,
-      'arrow-parens': 'off',
-      'no-param-reassign': 0,
-      'max-len': 'off',
-      'import/no-named-as-default': 'off'
-    },
-    env: {
-      'jest/globals': true
-    }
-  };
-  
+  extends: ["airbnb", "plugin:jest/recommended"],
+  plugins: ["jest"],
+  rules: {
+    semi: "off",
+    quotes: ["error", "double"],
+    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+    "react/prefer-stateless-function": 0,
+    "import/no-extraneous-dependencies": 0,
+    "import/no-unresolved": 0,
+    "import/extensions": 0,
+    "jsx-a11y/label-has-for": "off",
+    "comma-dangle": "off",
+    "object-curly-newline": "off",
+    "react/forbid-prop-types": "off",
+    "linebreak-style": 0,
+    "import/prefer-default-export": "off",
+    "arrow-parens": "off",
+    "no-nested-ternary": "off",
+    "no-plusplus": "off",
+    "no-underscore-dangle": [2, { allowAfterThis: true }],
+    "no-console": 0,
+    "no-param-reassign": 0,
+    "max-len": "off",
+    "import/no-named-as-default": "off"
+  },
+  env: {
+    "jest/globals": true
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+/* eslint "no-undef": "off" */
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
