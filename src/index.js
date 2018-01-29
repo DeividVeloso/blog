521798e53abfbf60eadf45584673376f8d56cce9
Deivid Veloso <veloso.deivid@gmail.com>
2018-01-29 13:18:46 -0200

    add BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
 import { store } from "./store/store";
 import Routes from "./routes";
@@ -8,7 +9,9 @@ import Routes from "./routes";
 /* eslint "no-undef": "off" */
 ReactDOM.render(
   <Provider store={store}>
-    <Routes />
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
