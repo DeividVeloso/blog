fcee42fcca61da9bc0158a71b5f7ba18d2906363
Deivid Veloso <veloso.deivid@gmail.com>
2018-02-02 14:45:00 -0200

    fix issue about relatives import css https://github.com/Semantic-Org/Semantic-UI-React/issues/1330

--- src/template/dependencies/index.js
+++ src/template/dependencies/index.js
@@ -1,7 +1,7 @@
-import './../../../node_modules/jquery/dist/jquery.min.js';
-import './../../../node_modules/bootstrap/dist/js/bootstrap.min.js';
-import './../../../node_modules/font-awesome/css/font-awesome.min.css';
-import './../../../node_modules/bootstrap/dist/css/bootstrap-reboot.min.css';
-import './../../../node_modules/bootstrap/dist/css/bootstrap-grid.min.css';
-import './../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap-reboot.min.css';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom.css';
