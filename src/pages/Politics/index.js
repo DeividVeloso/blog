6098aff0627831cb5d6181674eab14f8d73f5eb5
Deivid Veloso <veloso.deivid@gmail.com>
2018-07-02 18:02:03 -0300

    politics

---
+++ src/pages/Politics/index.js
@@ -0,0 +1,34 @@
+// Política de privacidade
+// Política de Privacidade da Acesso Soluções de Pagamento SA.
+// A Política de Privacidade foi criada para reafirmar o compromisso da Acesso Soluções de Pagamento SA com a segurança e a privacidade das informações coletadas dos usuários de seus produtos e serviços. Como essa política está sujeita a eventuais atualizações, recomendamos que ela seja consultada periodicamente.
+
+// Ao visitar nosso site e blog, você pode conhecer produtos e serviços que oferecemos ou obter informações e notícias sem precisar fornecer nenhuma informação pessoal. Mas, caso isso aconteça, esta política procura esclarecer como a Acesso Soluções de Pagamentos SA coleta e trata seus dados individuais.
+
+// 1 – Qualquer informação fornecida pelos usuários será coletada e guardada de acordo com os mais rígidos padrões de segurança e confiabilidade.
+
+// 2 – Todas as informações coletadas dos usuários trafegamde forma segura, utilizando processo de criptografia padrão da Internet.
+
+// 3 – Tal como outros websites, coletamos e utilizamos informação contida nos anúncios. A informação contida nos anúncios inclui o seu endereço IP (Internet Protocol), o seu ISP (Internet Service Provider, como o UOL, VIVO, TERRA ou outro), o browser que utilizou ao visitar o nosso website (como o Internet Explorer ou o Firefox), o tempo da sua visita e que páginas visitou dentro do nosso website.
+
+// 4 – As informações pessoais que nos forem fornecidas pelos usuários serão coletadas por meios éticos e legais. Essa coleta poderá ter um ou mais propósitos, sobre os quais nossos usuários serão informados.
+
+// 5 – Os usuários serão avisados sobre que dados seus estão sendo coletados, ficando a seu critério fornecê-los ou não, e serão informados também sobre as consequências de sua decisão.
+
+// 6 – A menos que tenhamos determinação legal ou judicial, as informações dos usuários jamais serão transferidas a terceiros ou usadas para finalidades diferentes daquelas para as quais foram coletadas.
+
+// 7 – O acesso às informações coletadas está restrito a funcionários autorizados para o uso adequado desses dados. Os funcionários que se utilizarem indevidamente dessas informações, ferindo nossa Política de Privacidade, estarão sujeitos às penalidades previstas em nosso processo disciplinar.
+
+// 8 – Manteremos a integridade das informações que nos forem fornecidas.
+
+// 9 – O Acesso Soluções de Pagamento SA. possui ligações para outros sites, os quais, a nosso ver, podem conter informações / ferramentas úteis para os nossos visitantes. A nossa política de privacidade não é aplicada a sites de terceiros, pelo que, caso visite outro site a partir do nosso deverá ler a politica de privacidade do mesmo. Desta forma os conteúdos e políticas de privacidade não são de responsabilidade da Acesso Soluções de Pagamento SA.
+
+// 10 – Será exigida de toda organização contratada para prover serviços de apoio, o cumprimento aos nossos padrões de privacidade e segurança da informação.
+
+// 11 – Para fins operações de crédito e gerenciamento de riscos, poderemos trocar informações sobre nossos clientes com fontes respeitáveis de referência, órgãos reguladores e serviços de compensação.
+
+// 12 – Eventualmente, poderemos utilizar cookies (*) para confirmar sua identidade, personalizar seu acesso e acompanhar a utilização de nosso website visando o aprimoramento de sua navegação e funcionalidade.
+
+// 13 – A Acesso Soluções de Pagamento coloca à disposição de seus usuários, canais de atendimento ao cliente, para esclarecer qualquer dúvida que possa surgir.
+
+// (*) Cookie: pequeno arquivo colocado em seu computador para entender os movimentos dentro dos websites como visita a páginas e anúncios. Cookies não armazenam informações pessoais sem que você as tenha fornecido e não coletam informações registradas em seu computador. A maioria dos browsers possibilita que o usuário, a qualquer instante, ative mecanismos para informá-lo quando os cookies estiverem acionados ou para evitar que sejam acionados, embora isso possa afetar a utilização de algumas funções de nosso site.
+
